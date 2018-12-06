13e007241d5984cda0bbd329a6d907a87fbb3bd7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 05:54:51 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -6,3 +6,20 @@ dim(data123)
 names(data123)
 x<-raw_data$Gender
 x
+
+#importing library for ggplot2 to generate barcharts
+library("ggplot2")
+#7)	Calculate the number of murders per state
+
+
+mergeData$numMurders <- as.integer(mergeData$population*mergeData$Murder/100000)
+murdersPerState <- mergeData[,c(2,3,4,10)]
+murdersPerState
+
+#generating a barchart for number of murders per state 
+
+
+bar1 <- ggplot(mergeData, aes(x=stateName, y=numMurders))
+bar1 <- bar1 + geom_col()
+bar1 <- bar1 + ggtitle("Bar chart for murders per each state")
+bar1
